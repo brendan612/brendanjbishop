ddaa12a987a936d94fd6a55675d5f0efa4491f13
Brendan <brendan612@gmail.com>
2020-08-16 20:15:03 -0500

    updated gitignore

---
+++ build/precache-manifest.5b467d07f2b8156976692508a3fdd988.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfb3dfbd3406c86ad6377fda9e75344",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e32e49c09e788b402f9a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ece2af047e10f16c78b6",
+    "url": "/static/js/2.f68cbe75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f68cbe75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e32e49c09e788b402f9a",
+    "url": "/static/js/main.bcb11a44.chunk.js"
+  },
+  {
+    "revision": "e5ed8471484e52d8bf8d",
+    "url": "/static/js/runtime-main.6b384e1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
