c0989de366c1c87cf311510405533be4dfd55ec6
Brendan <brendan612@gmail.com>
2022-01-21 23:02:07 -0800

    added _blank to links

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
   return (
     <div className='bjb__footer'>
       <div className="bjb__footer-links">
-        <a href='https://github.com/brendan612'><AiOutlineGithub /></a>
-        <a href='https://www.linkedin.com/in/brendan-bishop-729b54145/'><AiFillLinkedin /></a>
-        <a href='https://www.instagram.com/brendanjbishop/'><AiFillInstagram /></a>
+        <a href='https://github.com/brendan612' target="_blank"><AiOutlineGithub /></a>
+        <a href='https://www.linkedin.com/in/brendan-bishop-729b54145/' target="_blank"><AiFillLinkedin /></a>
+        <a href='https://www.instagram.com/brendanjbishop/' target="_blank"><AiFillInstagram /></a>
       </div>
     </div>
   );
